3f2fe5ae1007dff9b80efdbaf489a70e10633e96
HamiltonDiaz <54079326+HamiltonDiaz@users.noreply.github.com>
2023-11-09 22:03:54 -0500

    inicio sidebar admin

---
+++ src/layouts/dashboard/SideNav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SimpleBar from 'simplebar-react';
+
+const sideNav = props => {
+  return (
+    <SimpleBar>
+        
+    </SimpleBar>
+        
+  )
+}
+
+
+export default sideNav
