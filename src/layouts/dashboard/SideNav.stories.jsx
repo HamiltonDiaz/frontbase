6199a9a3c7b0677de15d0b778898d03877ea6832
HamiltonDiaz <54079326+HamiltonDiaz@users.noreply.github.com>
2023-11-22 07:47:32 -0500

    se inicia barra lateral

---
+++ src/layouts/dashboard/SideNav.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import SideNav from './SideNav';
+
+export default {
+    title: 'Menu Lateral',
+    component: SideNav,
+};
+
+export const SideNaveExample = () => <SideNav/>
